2c55597a8db182b5db24626e7ce1b78bda626878
mario <mmartin@42london.com>
2024-03-21 01:33:33 +0100

    update(ft_list_push_front): no check for head node

--- ft_list_push_front.s
+++ ft_list_push_front.s
@@ -4,25 +4,21 @@
         section .text
 ; rdi -> beginning of the list as t_list **
 ; rsi -> allocated data ready to be placed in a node at the beginning
+; r8  -> first node of linked list as t_list *
 ft_list_push_front:
         push    rdi
         push    rsi
         mov     rdi, nodesize
         call    malloc ; malloc node size (16 bytes)
-        cmp     rax, 0 ; malloc error
-        je      ret
         pop     rsi
         pop     rdi
+        cmp     rax, 0 ; malloc error
+        je      ret
         mov     qword [rax + nodedata], rsi ; new->data = data
-        cmp     qword [rdi], 0 ; check if current head is NULL
-        je      nullhead
         mov     r8, qword [rdi]
         mov     qword [rax + nodenext], r8
         mov     qword [rdi], rax
         jmp     ret
-nullhead:
-        mov     qword [rdi], rax
-        mov     qword [rax + nodenext], 0
 ret:
         xor     rax, rax
         ret
